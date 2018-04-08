38c7e747e6f911fddaac54f5b02c834cfe6cf7a7
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-04-07 23:20:16 -0500

    add function to randomly shuffle image array and display pics

--- me_app/src/App.js
+++ me_app/src/App.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
+
+import Wrapper from "./components/Wrapper";
+import ClickItem from "./components/ClickItem";
+// import Navbar from "./components/Navbar";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+import members from "./members.json";
+
 import './App.css';
 
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Section from "./components/Section";
-import Footer from "./components/Footer";
-// import members from "./members.json";
-
-console.log("Footer"+Footer)
-
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Header />
-        <Section />
-        <Footer />
-      </div>
-    );
-  }
+    state = {
+        members: members,
+        correctGuess: []
+    };
+
+    // record = 0;
+
+    guess = id => {
+        if(this.state.correctGuess.indexOf(id)===-1){
+            this.setState({
+                correctGuess: this.state.correctGuess.concat(id),
+                members: this.state.members.sort((a,b)=>{return Math.round(Math.random())===0? -1:1 })
+            })
+        }else{
+            this.setState({
+                correctGuess: [],
+                // record: Math.max(this.state.record, this.state.correctGuess.length),
+                members: this.state.members.sort((a,b)=>{return Math.round(Math.random())===0? -1:1 }),
+            })
+        }
+    };
+
+    render() {
+        return (
+            <Wrapper>
+            
+                {this.state.members.map(member=>(<ClickItem guess={this.guess} image={member.image} id={member.id} />))}                
+
+            </Wrapper>
+        )
+    }
 }
 
+
+// var randomSorted = arr.sort((a,b)=>{return Math.round(Math.random())==0? -1:1 })
 export default App;
