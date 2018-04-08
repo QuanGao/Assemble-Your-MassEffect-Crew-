ffd158e17c7697bd32e43992c0de9665ebaab673
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-04-08 14:49:26 -0500

    change msg color based on right or wrong

--- me_app/src/App.js
+++ me_app/src/App.js
@@ -17,15 +17,27 @@ class App extends Component {
     correctGuess = [];
     record = 0;
     score = 0;
+
+    correctState = 0;
+
+    message = "Click a crew member to begin!";
     shuffleArr = arr => arr.sort((a,b)=>{return Math.round(Math.random())===0? -1:1 });
     guess = id => {
         if(this.correctGuess.indexOf(id)===-1){
+
+            this.correctState = 1;
+
             this.score++;
             this.record = Math.max(this.record, this.score);
-            this.correctGuess.push(id)
+            this.correctGuess.push(id);
+            this.message = "You guessed correctly!"
         }else{
+
+            this.correctState = -1;
+
             this.score = 0;
             this.correctGuess = [];
+            this.message = "You guessed incorrectly!"
         }
         this.setState({
             members: this.shuffleArr(this.state.members)
@@ -35,9 +47,11 @@ class App extends Component {
     render() {
         return (
             <Wrapper>
-                <Navbar score={this.score} record={this.record}/>
+                
+                <Navbar correctState={this.correctState} score={this.score} record={this.record} message={this.message}/>
+
                 <Playzone>            
-                    {this.state.members.map(member=>(<ClickItem guess={this.guess} image={member.image} id={member.id} />))}                
+                    {this.state.members.map(member=>(<ClickItem key={member.name} guess={this.guess} image={member.image} id={member.id} />))}                
                 </Playzone>
             </Wrapper>
         )
--- me_app/src/components/Navbar/Navbar.js
+++ me_app/src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component }  from "react";
 import "./Navbar.css";
 
 const listStyle = {
@@ -6,24 +6,48 @@ const listStyle = {
     padding: "10px"
 }
 
-const Navbar = (props) => {
-    return (
-        <div className="container-fluid scoreboard">
-            <ul style={listStyle} className="row">
-                <li className="restart col-md-2">Start New</li>
-                <li className="info col-md-6">Click a crew member to begin!</li>
-                <li className="score col-md-4">Score: {props.score} | Top Score: {props.record}</li>    
-            </ul>
-        </div>
-  
-  )
-}
-export default Navbar;
+class Navbar extends Component {
 
-// {/* <nav classNameName="scoreboard container-fluid navbar-expand-lg navbar-dark">
-// <ul classNameName="navbar-nav">      
-//     <li><a classNameName="navbar-brand" href="/">Start New</a></li>
-//     <li><span>Click an image to begin!</span></li>
-//     <li><span>Score: 0 | Top Score: 0</span></li>        
-// </ul>
-// </nav>   */}
+    // messageStyle = {
+    //     color: ""
+    // };
+
+    flashColor = correctState=>{
+        console.log(correctState)
+        switch(correctState){
+            case 1:
+                return {color:"green"};
+            case -1:
+                return {color:"red"};
+            default:
+                return {color:""}
+        }
+    }
+
+    render(){
+        return (
+            <div className="container-fluid scoreboard">
+                <ul style={listStyle} className="row">
+                    <li className="restart col-md-2">Start New</li>
+                    <li className="message col-md-6" style={this.flashColor(this.props.correctState)}>{this.props.message}</li>
+                    <li className="score col-md-4">Score: {this.props.score} | Top Score: {this.props.record}</li>    
+                </ul>
+            </div>
+        )
+    }
+}
+// const Navbar = (props) => {
+//     return (
+//         <div className="container-fluid scoreboard">
+//             <ul style={listStyle} className="row">
+//                 <li className="restart col-md-2">Start New</li>
+//                 <li className="message col-md-6" style={
+                
+//                     }>{props.message}</li>
+//                 <li className="score col-md-4">Score: {props.score} | Top Score: {props.record}</li>    
+//             </ul>
+//         </div>
+  
+//   )
+// }
+export default Navbar;
