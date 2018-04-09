7b926aafe2d6cc03b3c777375e72bd7252969aba
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-04-08 22:49:15 -0500

    add responsive css styles

--- me_app/src/components/Header/Header.js
+++ me_app/src/components/Header/Header.js
@@ -5,8 +5,8 @@ import "./Header.css";
 const Header = () => {
     return (
         <header className="container-fluid">
-            <h1>Welcome Commander Shepard!</h1><br/>
-            <h4>Assemble crew by click on them, but don't click on any more than once!</h4><br/>
+            <h1>Welcome Commander Shepard!</h1>
+            <h4>Assemble crew by click on them, but don't click on any more than once!</h4>
         </header>)
 
 }
